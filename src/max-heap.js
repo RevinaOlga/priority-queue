d46067f3a752f7d953f19f626722dce337f1524d
Olga <revina.volga@gmail.ru>
2019-03-22 23:56:21 +0300

    feat: add solved few tasks

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,43 +2,56 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+		if (this.root ==null) {
+			return;
+		} else {
+			return this.root;
+		}
 	}
 
 	detachRoot() {
-		
+		this.root = null;
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+
 	}
 
 	isEmpty() {
-		
+		if (this.root == null) {
+			return true;
+		}
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (this.root == null) {
+			this.root = node;
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+
 	}
 
 	shiftNodeDown(node) {
--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,28 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.heap = new MaxHeap;
+		this.maxSize = maxSize;
 	}
 
 	push(data, priority) {
-
+		this.heap.push(data, priority);
+		if (this.heap.size() == 30) {
+			throw Error;
+		}
 	}
 
 	shift() {
-
+		this.heap.pop();
 	}
 
 	size() {
-
+		return this.length;
 	}
 
 	isEmpty() {
-		
+		return this.heap.parentNodes.length == 0;
 	}
 }
 
